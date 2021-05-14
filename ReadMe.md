bcf2953fa5d4e23a22aec90980858e5107386ba2
Prashant Singh <pjeet.0733@gmail.com>
2021-05-14 23:14:58 +0400

    Json reader

--- ReadMe.md
+++ ReadMe.md
@@ -11,3 +11,11 @@ Tests can be executed in parallel using:
 - [pom.xml](pom.xml)
 - [testng.xml](testng.xml)
 - [TestRunner.java](src/test/java/runner/TestRunner.java)
+
+### To Deploy Sample JSON Server
+- Setup Node
+- Install `npm i json-server` or `npm i json-server -g`
+- Create a [db.json](src/test/resources/Files/db.json) file
+- Run `json-server --watch db.json`
+- Access server on `localhost:3000`
+[Reference Link](https://www.npmjs.com/package/json-server)
--- src/main/java/filereader/JsonReaderExample1.java
+++ src/main/java/filereader/JsonReaderExample1.java
@@ -2,14 +2,23 @@ package filereader;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
+import static io.restassured.RestAssured.given;
+
 public class JsonReaderExample1 {
     public static void main(String[] args) throws IOException {
+
+        // To convert JSON response to byte array and use
+//        Response res = given().contentType(ContentType.JSON).when()
+//                .get("https://reqres.in/api/users");
+//        byte[] jsonData = res.asByteArray();
 
         byte[] jsonData = Files.readAllBytes(Paths.get("src/test/resources/Files/ComplexJSON1.json"));
         ObjectMapper objectMapper = new ObjectMapper();
